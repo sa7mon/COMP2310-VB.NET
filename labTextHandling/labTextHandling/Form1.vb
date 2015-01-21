bb5450d58d01a69d1a882f00b44ab3c16a8c1c12
Dan Salmon <dan.sa7mon@gmail.com>
2015-01-21 14:23:25 -0600

    Add rich text file load event on program load and add test rich text for file.

--- labTextHandling/labTextHandling/Form1.vb
+++ labTextHandling/labTextHandling/Form1.vb
@@ -6,6 +6,9 @@
     End Sub
 
     Private Sub frmTextHandling_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Set the first item as the default selected item.
         cmbxListItems.SelectedIndex = 0
+        'Rich Text file resides 2 levels up from the debug directory.
+        rtbInput.LoadFile(System.IO.Path.GetFullPath("..\..\rtfExample.rtf"))
     End Sub
 End Class
