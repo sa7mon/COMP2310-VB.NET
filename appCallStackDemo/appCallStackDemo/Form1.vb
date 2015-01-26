f00064bbeed137b2d37f737b6195baff04f3ac66
Dan Salmon <dan.sa7mon@gmail.com>
2015-01-26 14:45:30 -0600

    Add assert for unit testing.

--- appCallStackDemo/appCallStackDemo/Form1.vb
+++ appCallStackDemo/appCallStackDemo/Form1.vb
@@ -10,15 +10,20 @@
     Dim flagTest As Integer = 10
 
     Private Sub subroutineOne()
+        Debug.WriteLine("In subroutineOne()")
         txtMsg.Text = "In subroutineOne()" & vbCrLf
         subroutineTwo()
     End Sub
     Private Sub subroutineTwo()
+        Debug.WriteLine("In subroutineTwo()")
         txtMsg.Text &= "In subroutineTwo()" & vbCrLf
         subroutineThree()
     End Sub
     Private Sub subroutineThree()
+        Debug.WriteLine("In subroutineThree()")
         txtMsg.Text &= "In subroutineThree()" & vbCrLf
+        flagTest = -1
+        Debug.Assert(flagTest >= 0, "flagTest is less than zero! This is from the Debug.Assert message in subroutineThree()")
     End Sub
 
     Private Sub btnDo_Click(sender As Object, e As EventArgs) Handles btnDo.Click
