1376ab50c128717e7f362cd6d50dbb929a658fdf
Dan Salmon <dan.sa7mon@gmail.com>
2015-01-28 13:07:21 -0600

    Add comment block

--- prjVBApp/prjVBApp/Form1.vb
+++ prjVBApp/prjVBApp/Form1.vb
@@ -1,5 +1,9 @@
-﻿Public Class Form1
+﻿' Project: VB Application [GitHub link here]
+' Created by: Dan Salmon [https://danthesalmon.com]
+' Created on: 1/25/15
+' Updated on: 1/28/15
 
+Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         cmbPartNo.SelectedIndex = 0
     End Sub
