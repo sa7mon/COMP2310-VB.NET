cfbce6050946de2849da1d6ecf17388678f483c5
Dan Salmon <dan.sa7mon@gmail.com>
2015-01-28 13:23:44 -0600

    Add functionality to change lblPartName on cmbPartNo_SelectedIndex change.

--- prjVBApp/prjVBApp/Form1.vb
+++ prjVBApp/prjVBApp/Form1.vb
@@ -3,6 +3,9 @@
 ' Created on: 1/25/15
 ' Updated on: 1/28/15
 
+
+'Part 1: http://offset.gobizkorea.com/att/cat/akbgks/tp_html/img/akbgks_cat_755148_small_img_2.gif
+
 Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' On form load: Select a drop-down item
@@ -22,6 +25,17 @@ Public Class Form1
 
     Private Sub cmbPartNo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbPartNo.SelectedIndexChanged
         'Populate labels when the selection in the combo box changes
-
+        If cmbPartNo.SelectedItem.ToString = "Pick a Part No." Then
+            lblPartName.Text = ""
+        End If
+        If cmbPartNo.SelectedItem.ToString = "12345" Then
+            lblPartName.Text = "Bearing Assembly #1"
+        End If
+        If cmbPartNo.SelectedItem.ToString = "23456" Then
+            lblPartName.Text = "Part #2"
+        End If
+        If cmbPartNo.SelectedItem.ToString = "34567" Then
+            lblPartName.Text = "Part #3"
+        End If
     End Sub
 End Class
