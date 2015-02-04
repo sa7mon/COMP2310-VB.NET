9ef8b942e690aa9a9e4915e46d48a447f5a9d280
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-04 14:03:13 -0600

    Create cleanISBN() function and

--- prjISBN/prjISBN/showISBN.vb
+++ prjISBN/prjISBN/showISBN.vb
@@ -5,4 +5,22 @@
 
 Public Class frmShowISBN
 
+    Public Function cleanISBN(input)
+        Dim cleanedISBN As String = ""
+
+        For Each c As Char In input
+            ' Loop through each characted in the input string
+            ' Check for any character that's not a number and ignore it
+            If (Char.IsNumber(c)) Then
+                'Yep, it's a number alright. Add it to the string we're going to return
+                cleanedISBN += c
+            End If
+        Next
+
+        Return cleanedISBN
+    End Function
+
+    Private Sub txtISBN_TextChanged(sender As Object, e As EventArgs) Handles txtISBN.TextChanged
+        lblValid.Text = IIf(txtISBN.Text.Length > 12, cleanISBN(txtISBN.Text), "Not an ISBN")
+    End Sub
 End Class
