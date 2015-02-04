0d1c725e0bc2a16927a2520265eb4a4a9949cefb
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-04 13:41:32 -0600

    Add elements and name them

--- prjISBN/prjISBN/showISBN.vb
+++ prjISBN/prjISBN/showISBN.vb
@@ -1,3 +1,8 @@
-﻿Public Class showISBN
+﻿''File: showISBN.vb (https://github.com/sa7mon/COMP2310-VB.NET)
+''Created By: Dan Salmon (https://danthesalmon.com)
+''Created On: 2/4/15
+''Last Updated: 2/4/15
+
+Public Class frmShowISBN
 
 End Class
