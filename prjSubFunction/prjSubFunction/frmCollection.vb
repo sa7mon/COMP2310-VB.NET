294d8ea0cfb132df8be682819384248dae723b1c
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-18 14:28:55 -0600

    Add comment block to form

--- prjSubFunction/prjSubFunction/frmCollection.vb
+++ prjSubFunction/prjSubFunction/frmCollection.vb
@@ -1,3 +1,7 @@
-﻿Public Class frmCollection
+﻿'    Project: subFunction (https://github.com/sa7mon/COMP2310-VB.NET)
+' Created By: Dan Salmon (https://danthesalmon.com)
+' Created On: 2/18/15
+' Updated On: 2/18/15
+Public Class frmCollection
 
 End Class
