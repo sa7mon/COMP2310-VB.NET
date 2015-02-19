178780dc86455c4c25a3909a2e35187805eacc84
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-18 19:47:11 -0600

    Add functionality to btnReadXML

--- labXMLParsing/labXMLParsing/frmParseXML.vb
+++ labXMLParsing/labXMLParsing/frmParseXML.vb
@@ -1,3 +1,12 @@
 ﻿Public Class frmParseXML
 
+    Private Sub btnReadXML_Click(sender As Object, e As EventArgs) Handles btnReadXML.Click
+        Dim xmlPath As String = "..\..\authors.xml"
+
+        ' Read the XML file into the invisible dataset
+        datasetAuthors.ReadXml(xmlPath)
+
+        gridData.DataSource = datasetAuthors
+        gridData.DataMember = "authors"
+    End Sub
 End Class
