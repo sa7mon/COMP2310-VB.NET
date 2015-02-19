a256080c2caf1da821de98da79fa7b065843305e
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-18 20:12:33 -0600

    Add readXML function. Need to test.

--- prjSubFunction/prjSubFunction/frmCollection.Designer.vb
+++ prjSubFunction/prjSubFunction/frmCollection.Designer.vb
@@ -22,17 +22,41 @@ Partial Class frmCollection
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.dsRecords = New System.Data.DataSet()
+        Me.DataTable1 = New System.Data.DataTable()
+        Me.DataColumn1 = New System.Data.DataColumn()
+        CType(Me.dsRecords, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataTable1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'dsRecords
+        '
+        Me.dsRecords.DataSetName = "NewDataSet"
+        Me.dsRecords.Tables.AddRange(New System.Data.DataTable() {Me.DataTable1})
+        '
+        'DataTable1
+        '
+        Me.DataTable1.Columns.AddRange(New System.Data.DataColumn() {Me.DataColumn1})
+        Me.DataTable1.TableName = "Table1"
+        '
+        'DataColumn1
+        '
+        Me.DataColumn1.ColumnName = "Column1"
         '
         'frmCollection
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(688, 508)
         Me.Name = "frmCollection"
         Me.Text = "Collection"
+        CType(Me.dsRecords, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataTable1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents dsRecords As System.Data.DataSet
+    Friend WithEvents DataTable1 As System.Data.DataTable
+    Friend WithEvents DataColumn1 As System.Data.DataColumn
 
 End Class
--- prjSubFunction/prjSubFunction/modFunctions.vb
+++ prjSubFunction/prjSubFunction/modFunctions.vb
@@ -1,4 +1,15 @@
 ﻿Module modFunctions
+    Public Sub readXML(xmlPath As String, xmlDataSet As DataSet, grid As DataGridView, member As String)
+        ' Read the XML file into the invisible dataset
+        xmlDataSet.ReadXml(xmlPath)
+
+        'Change the source of data on the grid to the invisible dataset
+        grid.DataSource = xmlDataSet
+
+        'Tell the grid which element to not list
+        grid.DataMember = member
+    End Sub
+
     Public Function dashConCat() As String
         Dim strResult = ""
 
