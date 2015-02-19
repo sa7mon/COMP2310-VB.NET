f0d8bd519c5376ea9c5254de1a6b0207b63a4144
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-18 19:56:45 -0600

    Add functionality to read schema button

--- labXMLParsing/labXMLParsing/frmParseXML.vb
+++ labXMLParsing/labXMLParsing/frmParseXML.vb
@@ -9,4 +9,10 @@
         gridData.DataSource = datasetAuthors
         gridData.DataMember = "authors"
     End Sub
+
+    Private Sub btnShowSchema_Click(sender As Object, e As EventArgs) Handles btnShowSchema.Click
+        Dim swXML As New System.IO.StringWriter()
+        datasetAuthors.WriteXmlSchema(swXML)
+        txtSchema.Text = datasetAuthors.ToString
+    End Sub
 End Class
