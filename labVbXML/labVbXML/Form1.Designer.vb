582924579af694905858a4d3ccbd585f460b8fcb
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-23 13:40:28 -0600

    Add functionality to btnRead

--- labVbXML/labVbXML/Form1.Designer.vb
+++ labVbXML/labVbXML/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,65 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblResults = New System.Windows.Forms.Label()
+        Me.txtParagraph = New System.Windows.Forms.TextBox()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.btnXPath = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblResults
+        '
+        Me.lblResults.AutoSize = True
+        Me.lblResults.Location = New System.Drawing.Point(12, 9)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(42, 13)
+        Me.lblResults.TabIndex = 0
+        Me.lblResults.Text = "Results"
+        '
+        'txtParagraph
+        '
+        Me.txtParagraph.Location = New System.Drawing.Point(60, 12)
+        Me.txtParagraph.Multiline = True
+        Me.txtParagraph.Name = "txtParagraph"
+        Me.txtParagraph.Size = New System.Drawing.Size(520, 265)
+        Me.txtParagraph.TabIndex = 1
+        '
+        'btnRead
+        '
+        Me.btnRead.Location = New System.Drawing.Point(590, 23)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(101, 28)
+        Me.btnRead.TabIndex = 2
+        Me.btnRead.Text = "Use XMLReader"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'btnXPath
+        '
+        Me.btnXPath.Location = New System.Drawing.Point(590, 88)
+        Me.btnXPath.Name = "btnXPath"
+        Me.btnXPath.Size = New System.Drawing.Size(101, 23)
+        Me.btnXPath.TabIndex = 3
+        Me.btnXPath.Text = "Use XPath"
+        Me.btnXPath.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(699, 289)
+        Me.Controls.Add(Me.btnXPath)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.txtParagraph)
+        Me.Controls.Add(Me.lblResults)
+        Me.Name = "frmMain"
+        Me.Text = "Parsing XML"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents lblResults As System.Windows.Forms.Label
+    Friend WithEvents txtParagraph As System.Windows.Forms.TextBox
+    Friend WithEvents btnRead As System.Windows.Forms.Button
+    Friend WithEvents btnXPath As System.Windows.Forms.Button
 
 End Class
--- labVbXML/labVbXML/Form1.vb
+++ labVbXML/labVbXML/Form1.vb
@@ -1,3 +1,29 @@
-﻿Public Class frmMain
+﻿Imports System.Xml
+Imports System.Xml.XPath
+Imports System.Data
 
+Public Class frmMain
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        Dim docXML As XDocument = XDocument.Load("myXML.xml")
+        For Each node As XElement In docXML.Descendants("story")
+            txtParagraph.Text &= node.Element("paragraph").Value & vbCrLf
+        Next
+
+        txtParagraph.Text &= vbCrLf & "=============================" & vbCrLf
+
+        For Each node As XElement In docXML.Descendants("story")
+            Dim paragraphs As IEnumerable(Of XElement) = node.Descendants("paragraph")
+            For Each thisPara In paragraphs
+                txtParagraph.Text &= "Paragraph: " & thisPara.Value & vbCrLf
+            Next
+        Next
+
+        For Each node As XElement In docXML.Descendants("story")
+            Dim actors As IEnumerable(Of XElement) = node.Descendants("actor")
+            For Each thisActor In actors
+                txtParagraph.Text &= "Actor: " & thisActor.Value & vbCrLf
+            Next
+        Next
+    End Sub
 End Class
