5206c0f4a37a6208765e1be1905e94baa6eececa
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-22 18:59:16 -0600

    Use the function instead of hard-coding

--- prjSubFunction/prjSubFunction/frmCollection.vb
+++ prjSubFunction/prjSubFunction/frmCollection.vb
@@ -1,17 +1,13 @@
 ﻿'    Project: subFunction (https://github.com/sa7mon/COMP2310-VB.NET)
 ' Created By: Dan Salmon (https://danthesalmon.com)
 ' Created On: 2/18/15
-' Updated On: 2/18/15
+' Updated On: 2/22/15
 Public Class frmCollection
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'readXML("..\..\myCollection.xml", dsRecords, gridRecords, "record")
-        Dim xmlPath As String = "..\..\myCollection.xml"
 
-        ' Read the XML file into the invisible dataset
-        dsRecords.ReadXml(xmlPath)
+        'Call function from modFunctions
+        readXML("..\..\myCollection.xml", dsRecords, gridRecords, "record")
 
-        gridRecords.DataSource = dsRecords
-        gridRecords.DataMember = "record"
     End Sub
 End Class
