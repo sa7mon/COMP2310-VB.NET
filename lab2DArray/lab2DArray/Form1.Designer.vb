8edd9e40d2402c1730263cbda5b3f26891122240
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-23 14:04:01 -0600

    Switch from textbox to labels

--- lab2DArray/lab2DArray/Form1.Designer.vb
+++ lab2DArray/lab2DArray/Form1.Designer.vb
@@ -22,41 +22,90 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.txtResult = New System.Windows.Forms.TextBox()
         Me.btnGo = New System.Windows.Forms.Button()
+        Me.lblFirst = New System.Windows.Forms.Label()
+        Me.lblSecond = New System.Windows.Forms.Label()
+        Me.lblThird = New System.Windows.Forms.Label()
+        Me.lblFourth = New System.Windows.Forms.Label()
+        Me.lblFifth = New System.Windows.Forms.Label()
         Me.SuspendLayout()
-        '
-        'txtResult
-        '
-        Me.txtResult.Location = New System.Drawing.Point(12, 12)
-        Me.txtResult.Multiline = True
-        Me.txtResult.Name = "txtResult"
-        Me.txtResult.Size = New System.Drawing.Size(420, 263)
-        Me.txtResult.TabIndex = 0
         '
         'btnGo
         '
-        Me.btnGo.Location = New System.Drawing.Point(438, 12)
+        Me.btnGo.Location = New System.Drawing.Point(99, 114)
         Me.btnGo.Name = "btnGo"
-        Me.btnGo.Size = New System.Drawing.Size(114, 34)
+        Me.btnGo.Size = New System.Drawing.Size(191, 46)
         Me.btnGo.TabIndex = 1
         Me.btnGo.Text = "Go!"
         Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'lblFirst
+        '
+        Me.lblFirst.AutoSize = True
+        Me.lblFirst.Location = New System.Drawing.Point(12, 12)
+        Me.lblFirst.Name = "lblFirst"
+        Me.lblFirst.Size = New System.Drawing.Size(39, 13)
+        Me.lblFirst.TabIndex = 2
+        Me.lblFirst.Text = "Label1"
+        '
+        'lblSecond
+        '
+        Me.lblSecond.AutoSize = True
+        Me.lblSecond.Location = New System.Drawing.Point(84, 12)
+        Me.lblSecond.Name = "lblSecond"
+        Me.lblSecond.Size = New System.Drawing.Size(39, 13)
+        Me.lblSecond.TabIndex = 3
+        Me.lblSecond.Text = "Label2"
+        '
+        'lblThird
+        '
+        Me.lblThird.AutoSize = True
+        Me.lblThird.Location = New System.Drawing.Point(165, 12)
+        Me.lblThird.Name = "lblThird"
+        Me.lblThird.Size = New System.Drawing.Size(39, 13)
+        Me.lblThird.TabIndex = 4
+        Me.lblThird.Text = "Label3"
+        '
+        'lblFourth
+        '
+        Me.lblFourth.AutoSize = True
+        Me.lblFourth.Location = New System.Drawing.Point(251, 12)
+        Me.lblFourth.Name = "lblFourth"
+        Me.lblFourth.Size = New System.Drawing.Size(39, 13)
+        Me.lblFourth.TabIndex = 5
+        Me.lblFourth.Text = "Label4"
+        '
+        'lblFifth
+        '
+        Me.lblFifth.AutoSize = True
+        Me.lblFifth.Location = New System.Drawing.Point(336, 12)
+        Me.lblFifth.Name = "lblFifth"
+        Me.lblFifth.Size = New System.Drawing.Size(39, 13)
+        Me.lblFifth.TabIndex = 6
+        Me.lblFifth.Text = "Label5"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(560, 287)
+        Me.ClientSize = New System.Drawing.Size(411, 201)
+        Me.Controls.Add(Me.lblFifth)
+        Me.Controls.Add(Me.lblFourth)
+        Me.Controls.Add(Me.lblThird)
+        Me.Controls.Add(Me.lblSecond)
+        Me.Controls.Add(Me.lblFirst)
         Me.Controls.Add(Me.btnGo)
-        Me.Controls.Add(Me.txtResult)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents txtResult As System.Windows.Forms.TextBox
     Friend WithEvents btnGo As System.Windows.Forms.Button
+    Friend WithEvents lblFirst As System.Windows.Forms.Label
+    Friend WithEvents lblSecond As System.Windows.Forms.Label
+    Friend WithEvents lblThird As System.Windows.Forms.Label
+    Friend WithEvents lblFourth As System.Windows.Forms.Label
+    Friend WithEvents lblFifth As System.Windows.Forms.Label
 
 End Class
--- lab2DArray/lab2DArray/Form1.vb
+++ lab2DArray/lab2DArray/Form1.vb
@@ -1,3 +1,25 @@
 ﻿Public Class Form1
 
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim values(,) As String = New String(,) {{"ant", "aunt"},
+                         {"Sam", "Samantha"},
+                         {"clozapine", "quetiapine"},
+                         {"flomax", "volmax"},
+                         {"toradol", "tramadol"}}
+
+        ' Get bounds of the array.
+        Dim bound0 As Integer = values.GetUpperBound(0)
+        Dim bound1 As Integer = values.GetUpperBound(1)
+
+        ' Loop over all elements.
+        For i As Integer = 0 To bound0
+            For x As Integer = 0 To bound1
+                ' Get element.
+                Dim s1 As String = values(i, x)
+                Console.Write(s1)
+                Console.Write(" "c)
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
 End Class
