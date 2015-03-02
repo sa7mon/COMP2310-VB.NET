476a5116620ca4ff7dba0f8faca41221cabb8c3f
Dan Salmon <dan.sa7mon@gmail.com>
2015-03-02 14:20:05 -0600

    Create new object and finish lab.

--- labDataStructure/labDataStructure/Form1.vb
+++ labDataStructure/labDataStructure/Form1.vb
@@ -7,23 +7,33 @@
         Public lastName As String
         Public email As String
 
+        Public ReadOnly Property Name() As String
+            Get
+                Return firstName & " " & lastName
+            End Get
+        End Property
+
     End Structure
 
     Private objCustomer As Customer
+    Private objNewCustomer As Customer
 
     Private Sub btnListCustomers_Click(sender As Object, e As EventArgs) Handles btnListCustomers.Click
-
         objCustomer.firstName = "Jim"
         objCustomer.lastName = "Halpbert"
         objCustomer.email = "jim.halpbert@dundermiflin.com"
-        displayCustomer(objCustomer)
+
+        objNewCustomer.firstName = "Dan"
+        objNewCustomer.lastName = "Awesome"
+        objNewCustomer.email = "dan.awesome@email.com"
+        displayCustomer(objNewCustomer)
     End Sub
 
     Private Sub displayCustomer(ByVal cust As Customer)
+        txtName.Text = cust.Name
         txtFirstName.Text = cust.firstName
         txtLastName.Text = cust.lastName
         txtEmail.Text = cust.email
-        txtName.Text = cust.firstName + " " + cust.lastName
     End Sub
 
 End Class
