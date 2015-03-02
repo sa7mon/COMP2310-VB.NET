7dc59353ac9033f164931e254dcf1d9bd8736a58
Dan Salmon <dan.sa7mon@gmail.com>
2015-03-02 14:24:20 -0600

    Create new lab and create a new streamWriter

---
+++ labStreams/labStreams/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtWrite = New System.Windows.Forms.TextBox()
+        Me.btnWrite = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtWrite
+        '
+        Me.txtWrite.Location = New System.Drawing.Point(0, 0)
+        Me.txtWrite.Multiline = True
+        Me.txtWrite.Name = "txtWrite"
+        Me.txtWrite.Size = New System.Drawing.Size(608, 338)
+        Me.txtWrite.TabIndex = 0
+        '
+        'btnWrite
+        '
+        Me.btnWrite.Location = New System.Drawing.Point(179, 344)
+        Me.btnWrite.Name = "btnWrite"
+        Me.btnWrite.Size = New System.Drawing.Size(75, 23)
+        Me.btnWrite.TabIndex = 1
+        Me.btnWrite.Text = "Write"
+        Me.btnWrite.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(613, 401)
+        Me.Controls.Add(Me.btnWrite)
+        Me.Controls.Add(Me.txtWrite)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtWrite As System.Windows.Forms.TextBox
+    Friend WithEvents btnWrite As System.Windows.Forms.Button
+
+End Class
---
+++ labStreams/labStreams/Form1.vb
@@ -0,0 +1,9 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private myWriter As New StreamWriter("myFile.txt")
+    Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+        myWriter.WriteLine(txtWrite.Text)
+    End Sub
+End Class
