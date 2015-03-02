bf0f351a2052b21e6257d24073b17977941b03b9
Dan Salmon <dan.sa7mon@gmail.com>
2015-03-02 14:01:13 -0600

    Create vinyl structure

--- prjDataStructure/prjDataStructure/Form1.vb
+++ prjDataStructure/prjDataStructure/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class frmMain
+    Public Structure vinyl
+        Public album As String
+        Public artist As String
+        Public year As Integer
+    End Structure
+
+
 
 End Class
