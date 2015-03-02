4d0f84a050c22c8cb28ee2321fe82b0bfb1b05b3
Dan Salmon <dan.sa7mon@gmail.com>
2015-03-02 13:27:07 -0600

    Create new lab and add elements

---
+++ labDataStructure/labDataStructure/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Structure Customer
+        'Public variables
+
+        Public firstName As String
+        Public lastName As String
+        Public email As String
+
+    End Structure
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
