1ad81556bd72060bc065c084d308a5c7e5e5e42f
Dan Salmon <dan.sa7mon@gmail.com>
2015-03-16 14:27:40 -0500

    Add code to read all columns from db and output to datagrid

--- labSQLConnect/labSQLConnect/Form1.vb
+++ labSQLConnect/labSQLConnect/Form1.vb
@@ -4,7 +4,23 @@ Imports System.Data.OleDb
 Public Class frmMain
 
 
+
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim con = New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=userDatabase.mdb")
+        Dim strSQL As String = "SELECT * FROM userList"
+        Dim da As OleDbDataAdapter
+        Dim ds As New DataSet()
+        Try
+            con.Open()
+            da = New OleDbDataAdapter(strSQL, con)
+            da.TableMappings.Add("Table", "UserList")
+            da.Fill(ds)
+            con.Close()
+            dgResult.DataSource = ds
+            dgResult.DataMember = "UserList"
+        Catch ex As Exception
+
+        End Try
+
     End Sub
 End Class
