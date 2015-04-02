da19dd41ee4970330e489207f912d9937ac30eec
Dan Salmon <dan.sa7mon@gmail.com>
2015-04-02 09:04:41 -0500

    Add comments and print statement

--- prjSql/prjSQL.sql
+++ prjSql/prjSQL.sql
@@ -3,4 +3,27 @@
 -- Created by: Dan Salmon (https://danthesalmon.com/)
 -- Contact: dan.salmon@my.southcentral.edu
 -- Created on: 4/2/15
--- 
+
+PRINT 'Project: SQL by Dan Salmon';
+
+-- 1. Choose a database
+
+-- 2. Count employees
+
+-- 3. List Person Types
+
+-- 4. List Emails by Last Name
+
+-- 5. List Products 
+
+-- 6. Display Inventory using Grid
+
+-- 7. Add a new table
+
+-- 8. Populate Table with Data
+
+-- 9. Display Personal Days Information
+
+-- 10. Create own Personal SQL 
+
+-- 11. Most Important Technique
