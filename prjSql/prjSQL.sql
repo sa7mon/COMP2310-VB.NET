2e931a06c813ad1c58e8209c5863a2e20ab78e1c
Dan Salmon <dan.sa7mon@gmail.com>
2015-04-03 20:10:45 -0500

    Add Query num. 5

--- prjSql/prjSQL.sql
+++ prjSql/prjSQL.sql
@@ -34,15 +34,36 @@ HR wants a phone directory showing first name, last name, email. Due to the
 Using Person.Person and any other tables you need show the SQL statement 
 displaying the first name, last name, and email for all the people who's 
 last name starts with "L". The list should be in alphabetical order by last name.
-*/
+
 SELECT Person.Person.FirstName, Person.Person.LastName, Person.EmailAddress.EmailAddress
 FROM Person.Person
-INNER JOIN Person.EmailAddress
+FULL OUTER JOIN Person.EmailAddress
 ON Person.Person.BusinessEntityID=Person.Person.BusinessEntityID
-WHERE Person.Person.LastName LIKE 'L%';
+WHERE Person.Person.LastName LIKE 'L%'; 
+*/
 
 -- 5. List Products 
+/*
+You are building a mobile app for the sales department that will
+allow them to quickly look up the different models that are available based on categories.
+You plan to do a quick demo by putting this information in a grid. Later you will refine the
+interface using drop­downs and other niceties.
 
+Using the Production.Product and Production.ProductSubcategory tables, write a SQL
+statement that will show the name of each product and it's category.
+Use a JOIN
+Use an alias of prod for the Product table
+Use an alias of sub for the ProductSubcategory table
+Use an alias of Category for the ProductSubcategory.name field
+Order the grid by the ProductSubcategory.name field. (Tip: Use the full name, not
+the alias)
+
+*/
+SELECT prod.Name AS ProductName,sub.Name AS SubCategoryName
+FROM Production.Product AS prod
+JOIN Production.ProductSubCategory AS sub
+ON prod.ProductSubcategoryID=sub.ProductCategoryID
+ORDER BY sub.Name;
 -- 6. Display Inventory using Grid
 
 -- 7. Add a new table
