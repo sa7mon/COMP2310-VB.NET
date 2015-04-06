315b4dd362a96f9509e54132ff8eb3a7df2b873e
Dan Salmon <dan.sa7mon@gmail.com>
2015-04-06 13:39:32 -0500

    Create new form and add elements.

---
+++ labSqlAzure/labSqlAzure/Form1.Designer.vb
@@ -0,0 +1,307 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grpCurrent = New System.Windows.Forms.GroupBox()
+        Me.grpNewData = New System.Windows.Forms.GroupBox()
+        Me.dg = New System.Windows.Forms.DataGridView()
+        Me.lblUserIDlabel = New System.Windows.Forms.Label()
+        Me.lblUserID = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtUserID = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.grpCurrent.SuspendLayout()
+        Me.grpNewData.SuspendLayout()
+        CType(Me.dg, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'grpCurrent
+        '
+        Me.grpCurrent.Controls.Add(Me.lblEmail)
+        Me.grpCurrent.Controls.Add(Me.lblPassword)
+        Me.grpCurrent.Controls.Add(Me.Label2)
+        Me.grpCurrent.Controls.Add(Me.Label1)
+        Me.grpCurrent.Controls.Add(Me.lblUserID)
+        Me.grpCurrent.Controls.Add(Me.lblUserIDlabel)
+        Me.grpCurrent.Location = New System.Drawing.Point(12, 12)
+        Me.grpCurrent.Name = "grpCurrent"
+        Me.grpCurrent.Size = New System.Drawing.Size(598, 66)
+        Me.grpCurrent.TabIndex = 0
+        Me.grpCurrent.TabStop = False
+        Me.grpCurrent.Text = "Current Selection"
+        '
+        'grpNewData
+        '
+        Me.grpNewData.Controls.Add(Me.btnAdd)
+        Me.grpNewData.Controls.Add(Me.txtPhone)
+        Me.grpNewData.Controls.Add(Me.Label7)
+        Me.grpNewData.Controls.Add(Me.txtUserName)
+        Me.grpNewData.Controls.Add(Me.Label6)
+        Me.grpNewData.Controls.Add(Me.txtEmail)
+        Me.grpNewData.Controls.Add(Me.Label5)
+        Me.grpNewData.Controls.Add(Me.txtPassword)
+        Me.grpNewData.Controls.Add(Me.Label4)
+        Me.grpNewData.Controls.Add(Me.txtUserID)
+        Me.grpNewData.Controls.Add(Me.Label3)
+        Me.grpNewData.Location = New System.Drawing.Point(12, 246)
+        Me.grpNewData.Name = "grpNewData"
+        Me.grpNewData.Size = New System.Drawing.Size(705, 66)
+        Me.grpNewData.TabIndex = 1
+        Me.grpNewData.TabStop = False
+        Me.grpNewData.Text = "Add New Record"
+        '
+        'dg
+        '
+        Me.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dg.Location = New System.Drawing.Point(12, 84)
+        Me.dg.Name = "dg"
+        Me.dg.Size = New System.Drawing.Size(598, 150)
+        Me.dg.TabIndex = 0
+        '
+        'lblUserIDlabel
+        '
+        Me.lblUserIDlabel.AutoSize = True
+        Me.lblUserIDlabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUserIDlabel.Location = New System.Drawing.Point(46, 20)
+        Me.lblUserIDlabel.Name = "lblUserIDlabel"
+        Me.lblUserIDlabel.Size = New System.Drawing.Size(46, 13)
+        Me.lblUserIDlabel.TabIndex = 0
+        Me.lblUserIDlabel.Text = "UserID"
+        '
+        'lblUserID
+        '
+        Me.lblUserID.AutoSize = True
+        Me.lblUserID.Location = New System.Drawing.Point(46, 43)
+        Me.lblUserID.Name = "lblUserID"
+        Me.lblUserID.Size = New System.Drawing.Size(43, 13)
+        Me.lblUserID.TabIndex = 1
+        Me.lblUserID.Text = "            "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(208, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Password"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(370, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Email"
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(217, 43)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(43, 13)
+        Me.lblPassword.TabIndex = 4
+        Me.lblPassword.Text = "            "
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(370, 43)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(43, 13)
+        Me.lblEmail.TabIndex = 5
+        Me.lblEmail.Text = "            "
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(616, 84)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(101, 44)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save Changes"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(616, 190)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(101, 44)
+        Me.btnDelete.TabIndex = 3
+        Me.btnDelete.Text = "Delete Selected Record"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "UserID"
+        '
+        'txtUserID
+        '
+        Me.txtUserID.Location = New System.Drawing.Point(13, 34)
+        Me.txtUserID.Name = "txtUserID"
+        Me.txtUserID.Size = New System.Drawing.Size(100, 20)
+        Me.txtUserID.TabIndex = 1
+        Me.txtUserID.Text = "harry123"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(251, 34)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(100, 20)
+        Me.txtPassword.TabIndex = 3
+        Me.txtPassword.Text = "Please99##"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(248, 16)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Password"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(371, 34)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(100, 20)
+        Me.txtEmail.TabIndex = 5
+        Me.txtEmail.Text = "harPo@gmail.com"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(370, 16)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(32, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Email"
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(134, 34)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(100, 20)
+        Me.txtUserName.TabIndex = 7
+        Me.txtUserName.Text = "harry0999"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(131, 16)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(60, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "User Name"
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(482, 34)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(100, 20)
+        Me.txtPhone.TabIndex = 9
+        Me.txtPhone.Text = "123-987-6543"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(479, 12)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(38, 13)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "Phone"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(598, 16)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(101, 44)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "Add Record"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(727, 324)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.dg)
+        Me.Controls.Add(Me.grpNewData)
+        Me.Controls.Add(Me.grpCurrent)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.grpCurrent.ResumeLayout(False)
+        Me.grpCurrent.PerformLayout()
+        Me.grpNewData.ResumeLayout(False)
+        Me.grpNewData.PerformLayout()
+        CType(Me.dg, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents grpCurrent As System.Windows.Forms.GroupBox
+    Friend WithEvents lblEmail As System.Windows.Forms.Label
+    Friend WithEvents lblPassword As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblUserID As System.Windows.Forms.Label
+    Friend WithEvents lblUserIDlabel As System.Windows.Forms.Label
+    Friend WithEvents grpNewData As System.Windows.Forms.GroupBox
+    Friend WithEvents dg As System.Windows.Forms.DataGridView
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents txtPhone As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtUserID As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
